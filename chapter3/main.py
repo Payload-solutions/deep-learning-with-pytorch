c5575f50f238cd5d30fb9ba8147e69c01e1ac780
Payload <anegreiross@outlook.com>
2021-09-19 00:16:43 -0500

    ok

---
+++ chapter3/main.py
@@ -0,0 +1,43 @@
+"""First model implementation"""
+
+import numpy as np
+import torch
+
+
+# def np_arrays():
+#     vector = np.array([1, 2, 3, 4])
+#     matrix = np.array([[2, 3, 5], [7, 11, 13], [17, 19, 23]])
+#     tensor = np.array([
+#         [[2, 3, 5], [7, 11, 13], [17, 19, 23]],
+#         [[2, 3, 5], [7, 11, 13], [17, 19, 23]],
+#         [[2, 3, 5], [7, 11, 13], [17, 19, 23]]
+#     ])
+
+
+#     print(f"""vector => {vector} 
+    
+#     A matrix:
+#     {matrix}
+#     A Tensor:
+#      {tensor}""")
+
+
+def firsts_tensors():
+    """a = torch.ones(3)
+    print(a)
+    print(a[0])
+    print(a[2])
+    
+    for x in a:
+        print(x)"""
+
+
+
+def main():
+    firsts_tensors()
+
+
+
+
+if __name__ == "__main__":
+    main()
