819a499499d6f54712246b537d384ad2f6a21578
Payload <anegreiross@outlook.com>
2021-09-20 01:05:05 -0500

    still watching the chapter 4

--- chapter4/main.py
+++ chapter4/main.py
@@ -7,23 +7,48 @@ import imageio
 import os
 
 
-def working_data_():
+def working_data():
 
     batch_size = 3
     batch = torch.zeros(batch_size, 3, 256, 256)
-    data_dir = "../data/p1ch4/image-cats/"
+    data_dir = '../data/p1ch4/image-cats/'
 
     filenames = [name for name in os.listdir(data_dir) if os.path.splitext(name)[-1] == '.png']
 
 
     for i, filename in enumerate(filenames):
-        image_arr = imageio.imread(os.path.join(data_dir, filenames))
+        image_arr = imageio.imread(os.path.join(data_dir, filename))
         img_t = torch.from_numpy(image_arr)
         img_t = img_t.permute(2, 0, 1)
         img_t = img_t[:3]
         batch[i] = img_t
+    
+
+    """We mentionaed earlier htat neural networks usually work with
+    floating-points tensors as their input. Neural networks exibit
+    the best training performance when the input data ranges roughly
+    from 0 to 1, or from -1 to 1 (this is an effect of how their
+    building blocks are defined).
+    
+        So tipical thing we'll wanto to do is cast a tensor to floating-
+        pong ans normalize the values of the pixels. Casting to floating
+        point is easy, but normalizations is trickier, as ir depends on 
+        what range of input we decide should lie between 0 and 1 or -1 to 1
+        . One posibility is to just divide the values of the pixels by
+        255 (the maximun representable number in 8-bit unisged)
+    """
+    batch = batch.float()
+    batch /= 255.0
+
+    n_channels = batch.shape[1]
+    # image manipulation available
+    print(f" before touch the dataset \n\n{batch}")
+    for c in range(n_channels):
+        mean = torch.mean(batch[:, c])
+        std = torch.std(batch[:, c])
+        batch[:, c] = (batch[:, c] - mean) /std
+    print(f"after touch the dataset \n\n{batch}")
 def main():
-    image_arr = imageio.imread(PATH+"/dog.jpg")
     # print(image_arr)
     # print(image_arr.shape)
 
@@ -40,9 +65,11 @@ def main():
     # print(img)
     # print(out)
 
-    batch_size = 3
-    batch = torch.zeros(batch_size, 3, 256, 256, dtype=torch.uint8)
-    print(batch)
+    # batch_size = 3
+    # batch = torch.zeros(batch_size, 3, 256, 256, dtype=torch.uint8)
+    # print(batch)
+
+    working_data()
 
 if __name__ == "__main__":
     main()
---
+++ chapter4/volumetric_data.py
@@ -0,0 +1,10 @@
+"""Working with volumetric data"""
+
+
+"""We've learned how to load and preent 2D images, like the
+ones we take with a camera in some contexts, such as medical
+imaging applications involving, say, CT (computed tomography)
+scans, we typically deal with sequences of images stacked along
+the headto-foot axis, each corresponding to a slice across the 
+human body
+"""
