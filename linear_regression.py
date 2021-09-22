5f11b0d428b27a2549ccae3a5751b7439622ad21
Payload <anegreiross@outlook.com>
2021-09-22 16:29:42 -0500

    ok

--- linear_regression.py
+++ linear_regression.py
@@ -1,10 +1,11 @@
 """First implementation of neuron network"""
 
-
+from torch.utils.data import Dataset
 import torch
 from torch import nn
 from torch.autograd import Variable
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class LinearRegression(nn.Module):
@@ -15,6 +16,11 @@ class LinearRegression(nn.Module):
 
     def forward(self, x):
         return self.linear(x)
+
+
+
+class DataRegression(Dataset):...
+
 
 
 def main():
@@ -38,6 +44,41 @@ def main():
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 
+    for epoch in range(epochs):
+        # Converting inputs and labels to Variable
+        inputs = Variable(torch.from_numpy(x_train))
+        labels = Variable(torch.from_numpy(y_train))
 
+        """Clear gradient buffers because we don't want any
+        gradient from previous epoch to carry forward, dont 
+        want to cummulate
+        """
+        optimizer.zero_grad()
+
+        # get the output from the model, given the inputs
+        outputs = model(inputs)
+
+
+        # get the loss for the predicted output
+        loss = criterion(outputs, labels)
+        # print(loss)
+        
+        # get gradients w.r.t to parameters
+        loss.backward()
+
+        # update parameters
+        optimizer.step()
+
+        # print(f'epochs {epoch}, loss {loss.item()}')
+
+    with torch.no_grad(): # we don't need gradients in the testing phase
+        predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
+        print(predicted)
+    
+    plt.clf()
+    plt.plot(x_train, y_train, "go", label="True data", alpha=0.5)
+    plt.plot(x_train, predicted, '--', label="predictions", alpha=0.5)
+    plt.legend(loc='best')
+    plt.show()
 if __name__ == "__main__":
     main()
